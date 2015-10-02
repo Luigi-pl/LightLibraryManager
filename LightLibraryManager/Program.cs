1e2862d1ba8912d2e1762d827c1407fbed0de430
Luigi-pl <LukasZurawski@gmail.com>
2015-10-02 22:01:31 +0200

    Pierwsza wersja

---
+++ LightLibraryManager/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using LightLibraryManager.View;
+
+namespace LightLibraryManager
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główna pętla programu.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
